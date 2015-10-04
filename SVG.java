77e8f8367a249a2492b5f742a45bd0f8e1e01cb7
MoathAhmad <Abo.ahmad14@gmail.com>
2015-10-04 22:30:13 +0300

    Create SVG.java

---
+++ SVG.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class SVG {
+	
+	private LinkedList<String> rec;
+	private LinkedList<String> circle;
+	public SVG() {
+		rec = new LinkedList<String>();
+		circle =new LinkedList<String>();
+	}
+	public boolean read(String fileName) {
+		
+		
+	}	
+	public void write(String fileName) {
+		
+		
+	}	
+	public String query(String command) {
+		
+		
+		
+	}	
+}
