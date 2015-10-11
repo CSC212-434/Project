f3639a3ba150f4da4476110a8814ff6bcc70411d
MoathAhmad <Abo.ahmad14@gmail.com>
2015-10-11 14:01:34 +0300

    Update circle.java

--- circle.java
+++ circle.java
@@ -13,4 +13,35 @@ public class circle {
 		this.style = style;
 		
 	}
+		public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getStyle() {
+		return style;
+	}
+	public void setStyle(String style) {
+		this.style = style;
+	}
+	public String getCy() {
+		return cy;
+	}
+	public void setCy(String cy) {
+		this.cy = cy;
+	}
+	public String getCx() {
+		return cx;
+	}
+	public void setCx(String cx) {
+		this.cx = cx;
+	}
+	public String getR() {
+		return r;
+	}
+	public void setR(String r) {
+		this.r = r;
+	}
+	
 }
