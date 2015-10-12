957682894dd73bcc14034e85be9237fb6a74b0b4
Ziyad <hleb133@gmail.com>
2015-10-12 10:13:29 -0700

    Update SVG.java

--- SVG.java
+++ SVG.java
@@ -337,7 +337,18 @@ public class SVG {
 
 
 
-
+public static boolean isNumeric(String str)  
+{  
+  try  
+  {  
+    double d = Double.parseDouble(str);  
+  }  
+  catch(NumberFormatException nfe)  
+  {  
+    return false;  
+  }  
+  return true;  
+}
 
 
 
